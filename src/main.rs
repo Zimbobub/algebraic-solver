a3c0c9d9e95c631c5d3f08accb34429763e5079f
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2026-07-17 23:33:20 +1000

    start postfix stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod parser;
+mod postfix;
 mod tree;
 
 fn main() {
     let src = "1+x=2";
-    println!("{:?}", parser::parse(src));
+    let (left_src, right_src) = split(src).expect("No equals sign");
+
+    let left_infix = parser::parse(left_src);
+    let right_infix = parser::parse(right_src);
+
+    println!("{:?} {:?}", left_infix, right_infix);
+
+    let left_postfix = postfix::postfix(left_infix);
+    let right_postfix = postfix::postfix(right_infix);
+}
+
+
+
+fn split(src: &str) -> Option<(&str, &str)> {
+    return Some(src.split_at(src.find('=')?));
 }
---
+++ src/postfix.rs
@@ -0,0 +1,11 @@
+use crate::parser::Token;
+
+
+
+
+pub fn postfix(tokens: Vec<Token>) -> Vec<Token> {
+    let mut stack: Vec<Token> = Vec::new();
+
+
+    return tokens;
+}

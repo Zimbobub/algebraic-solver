94838b6a09f00084f7b8ac7aa074687a4f67dfcb
Zimbobub <89832179+Zimbobub@users.noreply.github.com>
2026-07-18 02:41:15 +1000

    postfix now works

--- src/postfix.rs
+++ src/postfix.rs
@@ -1,11 +1,45 @@
-use crate::parser::Token;
+use crate::parser::{Token, Tokens};
+use std::fmt::Write;
 
 
 
-
-pub fn postfix(tokens: Vec<Token>) -> Vec<Token> {
+pub fn postfix(infix: Tokens) -> Tokens {
     let mut stack: Vec<Token> = Vec::new();
+    let mut postfix: Vec<Token> = Vec::new();
 
+    for token in infix.0 {
+        println!("[{}] [{}]", Tokens(stack.clone()), Tokens(postfix.clone()));
+        match token {
+            Token::Variable(_) | Token::Number(_) => postfix.push(token),
+            Token::LBracket => stack.push(token),
+            Token::RBracket => {
+                // pop until '('
+                while let Some(top) = stack.pop_if(|tok| *tok != Token::LBracket) {
+                    postfix.push(top);
+                }
+                // ensure final pop is '('
+                if stack.pop().unwrap() != Token::LBracket { panic!("") }
+            },
+            // operator
+            _ => {
+                while let Some(top) = stack.last() && *top != Token::LBracket &&
+                    (
+                        top.precedence() > token.precedence() ||
+                        (top.precedence() == token.precedence() && !token.is_right_associative())
+                    ) {
+                    postfix.push(stack.pop().unwrap());
+                }
+                stack.push(token);
+            }
+        };
 
-    return tokens;
+    }
+
+    for remaining_operator in stack {
+        postfix.push(remaining_operator);
+    }
+
+    return Tokens(postfix);
 }
+
+
